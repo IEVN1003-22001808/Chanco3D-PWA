137aaefeac2def5e1fcb95ee27360138e8de6c91
IEVN1003-22001808 <georgeizac.skul@gmail.com>
2025-11-19 16:13:59 -0600

    Light & Dark MODE hechos (faltan módulos ADMIN)

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -9,4 +10,29 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class NavbarComponent {
+  isDarkMode = true; // Empezamos oscuros
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {
+    // Checar preferencia guardada (Opcional)
+    // const savedTheme = localStorage.getItem('theme');
+    // if (savedTheme === 'light') { this.isDarkMode = false; this.updateTheme(); }
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.updateTheme();
+  }
+
+  updateTheme() {
+    if (this.isDarkMode) {
+      this.renderer.removeClass(this.document.body, 'light-theme');
+      // localStorage.setItem('theme', 'dark');
+    } else {
+      this.renderer.addClass(this.document.body, 'light-theme');
+      // localStorage.setItem('theme', 'light');
+    }
+  }
 }
